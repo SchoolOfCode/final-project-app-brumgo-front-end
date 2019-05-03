c53a9760c50ea032f4ff6d5201c7fcccca603115
David Adams <daveadamsdev@gmail.com>
2019-05-03 14:47:11 +0100

    missing data completed

--- data/digbethPois.js
+++ data/digbethPois.js
@@ -1,5 +1,4 @@
 const pois = [
-
   {
     poiName: "st Martin in the Bullring",
     poiLocation: {
@@ -33,33 +32,36 @@ const pois = [
       lat: 52.4752029,
       lng: -1.884587
     },
-    {
-        poiName: "The Old Crown",
-        poiLocation: {
-            lat: 52.47481175,
-            lng: -1.88355471
-        },
-        poiTrivia:
-            "The Old Crown pub is the oldest secular building in Birmingham, England. It is Grade II listed, and claims to date back to 1368, retaining its 'black and white' timber frame, although almost all of the present building dates from the early 16th century."
+    poiTrivia:
+      "The Custard Factory complex is set in fifteen acres (60,000 m²) of factory buildings, originally constructed by Sir Alfred Frederick Bird (1849–1922), the son of Alfred Bird (1811–1878), the inventor of egg-free custard."
+  },
+  {
+    poiName: "The Old Crown",
+    poiLocation: {
+      lat: 52.47481175,
+      lng: -1.88355471
     },
-    {
-        poiName: "STEAMhouse (old Typhoo Tea Factory)",
-        poiLocation: {
-            lat: 52.476282,
-            lng: -1.889355
-        },
-        poiTrivia:
-            "One of the nation’s most loved teabag brands formed in Birmingham in the late 19th century. Until 1978, their factory was on Bordesley Street, where STEAMhouse is located today."
+    poiTrivia:
+      "The Old Crown pub is the oldest secular building in Birmingham, England. It is Grade II listed, and claims to date back to 1368, retaining its 'black and white' timber frame, although almost all of the present building dates from the early 16th century."
+  },
+  {
+    poiName: "STEAMhouse (old Typhoo Tea Factory)",
+    poiLocation: {
+      lat: 52.476282,
+      lng: -1.889355
     },
-    {
-        poiName: "Irish Centre",
-        poiLocation: {
-            lat: 52.474774,
-            lng: -1.885914
-        },
-        poiTrivia:
-            "Digbeth hosts the largest St. Patrick’s Day parade in the country. Appoximately 100,000 people line the streets of Digbeth one Sunday each March for the ever-popular Paddy’s Day Parade. Birmingham has always had a strong connection with Ireland, which is proven by the popularity of the parade!"
-    }
+    poiTrivia:
+      "One of the nation’s most loved teabag brands formed in Birmingham in the late 19th century. Until 1978, their factory was on Bordesley Street, where STEAMhouse is located today."
+  },
+  {
+    poiName: "Irish Centre",
+    poiLocation: {
+      lat: 52.474774,
+      lng: -1.885914
+    },
+    poiTrivia:
+      "Digbeth hosts the largest St. Patrick’s Day parade in the country. Appoximately 100,000 people line the streets of Digbeth one Sunday each March for the ever-popular Paddy’s Day Parade. Birmingham has always had a strong connection with Ireland, which is proven by the popularity of the parade!"
+  }
 ];
 
-export default pois
+export default pois;
