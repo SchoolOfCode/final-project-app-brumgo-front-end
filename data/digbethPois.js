46d955dd49d5bb36d666131540ca3f21073c0a84
David Adams <daveadamsdev@gmail.com>
2019-05-03 13:40:24 +0100

    digbeth info JSON started

---
+++ data/digbethPois.js
@@ -0,0 +1,28 @@
+const pois = [
+  {
+    poiName: "",
+    poiLocation: {
+      lat: 1,
+      lng: 1
+    },
+    poiTrivia: ""
+  },
+  {
+    poiName: "The Custard Factory",
+    poiLocation: {
+      lat: 52.4752029,
+      lng: -1.8845877
+    },
+    poiTrivia:
+      "The Custard Factory complex is set in fifteen acres of factory buildings, originally constructed by Sir Alfred Frederick Bird (1849–1922), the son of Alfred Bird (1811–1878), the inventor of egg-free custard."
+  },
+  {
+    poiName: "The Old Crown",
+    poiLocation: {
+      lat: 52.47481175,
+      lng: -1.88355471
+    },
+    poiTrivia:
+      "The Old Crown pub is the oldest secular building in Birmingham, England. It is Grade II listed, and claims to date back to 1368, retaining its 'black and white' timber frame, although almost all of the present building dates from the early 16th century."
+  }
+];
