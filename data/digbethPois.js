daa7b7cc466ec60494fd2757f0db5b79402655cd
David Adams <daveadamsdev@gmail.com>
2019-05-03 13:54:28 +0100

    4 JSON entries added

--- data/digbethPois.js
+++ data/digbethPois.js
@@ -8,6 +8,24 @@ const pois = [
     poiTrivia: ""
   },
   {
+    poiName: "Bull Ring Markets",
+    poiLocation: {
+      lat: 52.476499,
+      lng: -1.892838
+    },
+    poiTrivia:
+      "One of the oldest continuously trading markets in the world, The market legally began in 1154 when Peter de Bermingham, a local landowner, obtained a Charter of Marketing Rights from King Henry II."
+  },
+  {
+    poiName: "Battle of Birmingham",
+    poiLocation: {
+      lat: 52.47488727,
+      lng: -1.88596523
+    },
+    poiTrivia:
+      "During the Civil War the Battle of Birmingham was fought up into the town through Digbeth High Street on Easter Monday 3 April 1643"
+  },
+  {
     poiName: "The Custard Factory",
     poiLocation: {
       lat: 52.4752029,
