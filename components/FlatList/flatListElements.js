9af82a33144cc0b477362c4dc4316d0c9dd9f280
David Adams <daveadamsdev@gmail.com>
2019-05-07 09:35:28 +0100

    title and trivia for each item

--- components/FlatList/flatListElements.js
+++ components/FlatList/flatListElements.js
@@ -6,15 +6,11 @@ import { ListItem } from "react-native-elements";
 keyExtractor = (item, index) => index.toString();
 
 export default function FlatListElem() {
-    return (
-        <View>
-            {pois.map((item, idx) => 
-                (<ListItem
-                    key={idx}
-                    title={item.poiName}
-                    subtitle={item.poiTrivia}
-                />);
-            )}
-        </View>
-    );
+  return (
+    <View>
+      {pois.map((item, idx) => (
+        <ListItem key={idx} title={item.poiName} subtitle={item.poiTrivia} />
+      ))}
+    </View>
+  );
 }
