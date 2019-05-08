ad0906cb6302b3a78bad7989e82cdd49a9881af2
Manu Magalhaes <manu.magalhaes@ymail.com>
2019-05-08 16:37:53 +0100

    added custum icons for each POI category, bab

--- components/PinMarker/index.js
+++ components/PinMarker/index.js
@@ -5,19 +5,62 @@ import pois from "../../data/digbethPois";
 
 keyExtractor = (item, index) => index.toString();
 
+// const building = require("./Building.png");
+// const market = require("./Market.png");
+// const culture = require("./Culture.png");
+// const event = require("./Event.png");
+
+// function iconSetter(category) {
+//     switch (category) {
+//         case "Building":
+//             building;
+//             break;
+//         case "Market":
+//             market;
+//             break;
+//         case "Event":
+//             event;
+//             break;
+//         case "Culture":
+//             culture;
+//     }
+// }
+
 export default function PinMarker() {
     return (
         <View>
-            {pois.map((item, idx) => (
-                <MapView.Marker
-                    key={idx}
-                    title={item.poiName}
-                    coordinate={{
-                        latitude: item.poiLocation.lat,
-                        longitude: item.poiLocation.lng
-                    }}
-                />
-            ))}
+            {pois.map((item, idx) => {
+                let cat = item.category;
+                return (
+                    <MapView.Marker
+                        key={idx}
+                        title={item.poiName}
+                        icon={
+                            item.category === "Building"
+                                ? require("./Building.png")
+                                : item.category === "Culture"
+                                ? require("./Culture.png")
+                                : item.category === "Market"
+                                ? require("./Market.png")
+                                : require("./Event.png")
+                        }
+                        coordinate={{
+                            latitude: item.poiLocation.lat,
+                            longitude: item.poiLocation.lng
+                        }}
+                    />
+                );
+            })}
         </View>
     );
 }
+
+/* 
+item.category === "Building"
+                            ? require("./sand-beach-castle.png")
+                            : "Event"
+                            ? require("./battle.png")
+                            : "Market"
+                            ? require("./cart.png")
+                            : require("./dancer.png")
+*/
--- data/digbethPois.js
+++ data/digbethPois.js
@@ -1,67 +1,74 @@
 const pois = [
-  {
-    poiName: "st Martin in the Bullring",
-    poiLocation: {
-      lat: 52.477074,
-      lng: -1.893224
+    {
+        category: "Building",
+        poiName: "St Martin in the Bullring",
+        poiLocation: {
+            lat: 52.477074,
+            lng: -1.893224
+        },
+        poiTrivia:
+            "The present Victorian church was built on the site of a 13th-century predecessor, which was documented in 1263. The church was enlarged in medieval times and the resulting structure consisted of a lofty nave and chancel, north and south aisles and a northwest tower with spire."
     },
-    poiTrivia:
-      "The present Victorian church was built on the site of a 13th-century predecessor, which was documented in 1263. The church was enlarged in medieval times and the resulting structure consisted of a lofty nave and chancel, north and south aisles and a northwest tower with spire."
-  },
-  {
-    poiName: "Bull Ring Markets",
-    poiLocation: {
-      lat: 52.476499,
-      lng: -1.892838
+    {
+        category: "Market",
+        poiName: "Bull Ring Markets",
+        poiLocation: {
+            lat: 52.476499,
+            lng: -1.892838
+        },
+        poiTrivia:
+            "One of the oldest continuously trading markets in the world, The market legally began in 1154 when Peter de Bermingham, a local landowner, obtained a Charter of Marketing Rights from King Henry II."
     },
-    poiTrivia:
-      "One of the oldest continuously trading markets in the world, The market legally began in 1154 when Peter de Bermingham, a local landowner, obtained a Charter of Marketing Rights from King Henry II."
-  },
-  {
-    poiName: "Battle of Birmingham",
-    poiLocation: {
-      lat: 52.47488727,
-      lng: -1.88596523
+    {
+        category: "Event",
+        poiName: "Battle of Birmingham",
+        poiLocation: {
+            lat: 52.47488727,
+            lng: -1.88596523
+        },
+        poiTrivia:
+            "During the Civil War the Battle of Birmingham was fought up into the town through Digbeth High Street on Easter Monday 3 April 1643"
     },
-    poiTrivia:
-      "During the Civil War the Battle of Birmingham was fought up into the town through Digbeth High Street on Easter Monday 3 April 1643"
-  },
-  {
-    poiName: "The Custard Factory",
-    poiLocation: {
-      lat: 52.4752029,
-      lng: -1.884587
+    {
+        category: "Building",
+        poiName: "The Custard Factory",
+        poiLocation: {
+            lat: 52.4752029,
+            lng: -1.884587
+        },
+        poiTrivia:
+            "The Custard Factory complex is set in fifteen acres (60,000 m²) of factory buildings, originally constructed by Sir Alfred Frederick Bird (1849–1922), the son of Alfred Bird (1811–1878), the inventor of egg-free custard."
     },
-    poiTrivia:
-      "The Custard Factory complex is set in fifteen acres (60,000 m²) of factory buildings, originally constructed by Sir Alfred Frederick Bird (1849–1922), the son of Alfred Bird (1811–1878), the inventor of egg-free custard."
-  },
-  {
-    poiName: "The Old Crown",
-    poiLocation: {
-      lat: 52.47481175,
-      lng: -1.88355471
+    {
+        category: "Building",
+        poiName: "The Old Crown",
+        poiLocation: {
+            lat: 52.47481175,
+            lng: -1.88355471
+        },
+        poiTrivia:
+            "The Old Crown pub is the oldest secular building in Birmingham, England. It is Grade II listed, and claims to date back to 1368, retaining its 'black and white' timber frame, although almost all of the present building dates from the early 16th century."
     },
-    poiTrivia:
-      "The Old Crown pub is the oldest secular building in Birmingham, England. It is Grade II listed, and claims to date back to 1368, retaining its 'black and white' timber frame, although almost all of the present building dates from the early 16th century."
-  },
-  {
-    poiName: "STEAMhouse (old Typhoo Tea Factory)",
-    poiLocation: {
-      lat: 52.476282,
-      lng: -1.889355
+    {
+        category: "Building",
+        poiName: "Old Typhoo Tea Factory",
+        poiLocation: {
+            lat: 52.478311,
+            lng: -1.887021
+        },
+        poiTrivia:
+            "One of the nation’s most loved teabag brands formed in Birmingham in the late 19th century. The building survived bombing during the Second World War, but was eventually closed in 1978."
     },
-    poiTrivia:
-      "One of the nation’s most loved teabag brands formed in Birmingham in the late 19th century. Until 1978, their factory was on Bordesley Street, where STEAMhouse is located today."
-  },
-  {
-    poiName: "Irish Centre",
-    poiLocation: {
-      lat: 52.474774,
-      lng: -1.885914
-    },
-    poiTrivia:
-      "Digbeth hosts the largest St. Patrick’s Day parade in the country. Appoximately 100,000 people line the streets of Digbeth one Sunday each March for the ever-popular Paddy’s Day Parade. Birmingham has always had a strong connection with Ireland, which is proven by the popularity of the parade!"
-  }
+    {
+        category: "Culture",
+        poiName: "Irish Centre",
+        poiLocation: {
+            lat: 52.474774,
+            lng: -1.885914
+        },
+        poiTrivia:
+            "Digbeth hosts the largest St. Patrick’s Day parade in the country. Appoximately 100,000 people line the streets of Digbeth one Sunday each March for the ever-popular Paddy’s Day Parade. Birmingham has always had a strong connection with Ireland, which is proven by the popularity of the parade!"
+    }
 ];
 
 export default pois;
