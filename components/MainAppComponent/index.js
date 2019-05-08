442b7544cddbca9b5287044879a6c0fc5a8e2efb
David Adams <daveadamsdev@gmail.com>
2019-05-08 12:02:11 +0100

    layout 001 implemented and working

--- components/MainAppComponent/index.js
+++ components/MainAppComponent/index.js
@@ -4,12 +4,14 @@ import { StyleSheet, View } from "react-native";
 import HeaderSection from "../HeaderSection";
 import FooterSection from "../FooterSection";
 import FlatList from "../FlatList";
+import MapDisplay from "../MapDisplay";
 
 export default class MainAppComponent extends Component {
   render() {
     return (
       <View style={styles.container}>
         <HeaderSection />
+        <MapDisplay />
         <FlatList />
         <FooterSection />
       </View>
