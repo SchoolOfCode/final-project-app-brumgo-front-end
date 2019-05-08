c41353f89eefc58cc9c903bcabc077c2012c6062
David Adams <daveadamsdev@gmail.com>
2019-05-08 16:39:26 +0100

    initial navigation logic implemented

--- App.js
+++ App.js
@@ -9,17 +9,58 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
+import LoadingPage from "./components/LoadingPage";
 import MainAppComponent from "./components/MainAppComponent";
+import StorybookUIRoot from "./storybook";
 
 export default class App extends Component {
-  // const mapCenter =
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "LoadingPage"
+    };
+  }
+
+  handleNavigation = destination =>
+    this.setState(state => ({
+      ...state,
+      view: destination
+    }));
 
   render() {
-    return (
-      <View style={styles.container}>
-        <MainAppComponent />
-      </View>
-    );
+    switch (this.state.view) {
+      case "LoadingPage":
+        return (
+          <>
+            <LoadingPage handleNavigation={this.handleNavigation} />
+          </>
+        );
+        break;
+      case "Storybook":
+        return (
+          <>
+            <StorybookUIRoot />
+          </>
+        );
+        break;
+      case "MainView":
+        return (
+          <>
+            <MainAppComponent />
+          </>
+        );
+        break;
+      default:
+        <>
+          <Text>Invalid View</Text>
+        </>;
+    }
+
+    {
+      /* // <View style={styles.container}>
+      //   <MainAppComponent />
+      // </View> */
+    }
   }
 }
 
