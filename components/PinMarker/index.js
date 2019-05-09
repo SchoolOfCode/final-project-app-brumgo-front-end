9d8685783957a04389643991ca7e236015ed1473
David Adams <daveadamsdev@gmail.com>
2019-05-09 11:37:56 +0100

    bug fixing on wrong branch (sowwy)

--- components/PinMarker/index.js
+++ components/PinMarker/index.js
@@ -3,33 +3,33 @@ import { View, StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import pois from "../../data/digbethPois";
 
+const iconSet = {
+  Building: require("./Building.png"),
+  Culture: require("./Culture.png"),
+  Market: require("./Market.png"),
+  Event: require("./Event.png")
+};
+
 keyExtractor = (item, index) => index.toString();
 
 export default function PinMarker() {
-    return (
-        <View>
-            {pois.map((item, idx) => {
-                let cat = item.category;
-                return (
-                    <MapView.Marker
-                        key={idx}
-                        title={item.poiName}
-                        icon={
-                            item.category === "Building"
-                                ? require("./Building.png")
-                                : item.category === "Culture"
-                                ? require("./Culture.png")
-                                : item.category === "Market"
-                                ? require("./Market.png")
-                                : require("./Event.png")
-                        }
-                        coordinate={{
-                            latitude: item.poiLocation.lat,
-                            longitude: item.poiLocation.lng
-                        }}
-                    />
-                );
-            })}
-        </View>
-    );
+  return (
+    <View>
+      {pois.map((item, idx) => {
+        return (
+          <MapView.Marker
+            key={idx}
+            title={item.poiName}
+            image={iconSet[item.category]}
+            coordinate={{
+              latitude: item.poiLocation.lat,
+              longitude: item.poiLocation.lng
+            }}
+          >
+          <Teaxt>HI</Teaxt>
+          <MapView.Marker />
+        );
+      })}
+    </View>
+  );
 }
