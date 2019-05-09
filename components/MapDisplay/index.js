938c38b4257a868295995f8051f3b5f30e2f0356
David Adams <daveadamsdev@gmail.com>
2019-05-09 13:48:31 +0100

    map styled

--- components/MapDisplay/index.js
+++ components/MapDisplay/index.js
@@ -5,6 +5,14 @@ import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import mapStyle from "./mapstyle";
 import PinMarker from "../PinMarker";
 
+const initialPosition = {
+  // initial region set to Bileto
+  latitude: 52.475149,
+  longitude: -1.884333,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.0071
+};
+
 export default function MapDisplay() {
   return (
     <MapView
@@ -12,13 +20,7 @@ export default function MapDisplay() {
       provider={PROVIDER_GOOGLE}
       style={styles.map}
       showsUserLocation={true}
-      initialRegion={{
-        // initial region set to Bileto
-        latitude: 52.475149,
-        longitude: -1.884333,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.0071
-      }}
+      initialRegion={initialPosition}
     >
       <PinMarker />
     </MapView>
