ca8cf66fb732673d6b7fc619e62f9a2cfd9e204c
Manu Magalhaes <manu.magalhaes@ymail.com>
2019-05-10 11:41:24 +0100

    imported  header to navigation but need to get rid of white line

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-// import React from "react";
+import React from "react";
 // import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import LoadingPage from "./components/LoadingPage";
 import MainAppComponent from "./components/MainAppComponent";
 import StoryBook from "./storybook";
+import Header from "./components/HeaderSection";
 
 const AppNavigator = createStackNavigator(
-  {
-    Home: LoadingPage,
-    Main: MainAppComponent,
-    StoryBook: StoryBook
-  },
-  {
-    initialRouteName: "Home"
-  }
+    {
+        Home: LoadingPage,
+        Main: MainAppComponent,
+        StoryBook: StoryBook
+    },
+    {
+        initialRouteName: "Home",
+        defaultNavigationOptions: {
+            header: <Header />
+        }
+    }
 );
 
 export default createAppContainer(AppNavigator);
