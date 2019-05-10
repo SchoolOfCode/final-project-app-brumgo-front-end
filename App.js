4b1eb47ec60855406554c6dc346d4895cd377437
David Adams <daveadamsdev@gmail.com>
2019-05-10 11:09:33 +0100

    navigation between launch page, main page, and storybook, sorted :thumbs up:

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { View, Text } from "react-native";
+// import React from "react";
+// import { View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import LoadingPage from "./components/LoadingPage";
 import MainAppComponent from "./components/MainAppComponent";
+import StoryBook from "./storybook";
 
 const AppNavigator = createStackNavigator(
-    {
-        Home: LoadingPage,
-        Main: MainAppComponent
-    },
-    {
-        initialRouteName: "Home"
-    }
+  {
+    Home: LoadingPage,
+    Main: MainAppComponent,
+    StoryBook: StoryBook
+  },
+  {
+    initialRouteName: "Home"
+  }
 );
 
 export default createAppContainer(AppNavigator);
