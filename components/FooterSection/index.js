fcdf31a503310c97573cc1cc1aa044f370d06d20
Manu Magalhaes <manu.magalhaes@ymail.com>
2019-05-15 12:46:24 +0100

    search button shows draft search box

--- components/FooterSection/index.js
+++ components/FooterSection/index.js
@@ -1,39 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Footer, FooterTab, Button, Icon } from "native-base";
+import Search from "../Search";
 
-export default class FooterSection extends Component {
-  render() {
+export default function FooterSection() {
+    const [showSearch, setShowSearch] = useState(false);
+
     return (
-      <Footer>
-        <FooterTab style={{ backgroundColor: "#D3D3D3", height: "100%" }}>
-          <Button>
-            <Icon name="home" style={{ color: "#FFFFFF" }} />
-          </Button>
-          <Button>
-            <Icon name="search" style={{ color: "#FFFFFF" }} />
-          </Button>
-          <Button
-            style={{
-              height: 70,
-              width: 70,
-              bottom: 8,
-              borderWidth: 3,
-              borderColor: "#FFFFFF",
-              borderRadius: 35,
-              backgroundColor: "#FCC133"
-            }}
-            active
-          >
-            <Icon active name="navigate" />
-          </Button>
-          <Button>
-            <Icon name="camera" style={{ color: "#FFFFFF" }} />
-          </Button>
-          <Button>
-            <Icon name="person" style={{ color: "#FFFFFF" }} />
-          </Button>
-        </FooterTab>
-      </Footer>
+        <>
+            {showSearch ? <Search /> : null}
+            <Footer>
+                <FooterTab
+                    style={{ backgroundColor: "#D3D3D3", height: "100%" }}
+                >
+                    <Button>
+                        <Icon name="home" style={{ color: "#FFFFFF" }} />
+                    </Button>
+                    <Button onPress={() => setShowSearch(!showSearch)}>
+                        <Icon name="search" style={{ color: "#FFFFFF" }} />
+                    </Button>
+                    <Button
+                        style={{
+                            height: 70,
+                            width: 70,
+                            bottom: 8,
+                            borderWidth: 3,
+                            borderColor: "#FFFFFF",
+                            borderRadius: 35,
+                            backgroundColor: "#FCC133"
+                        }}
+                        active
+                    >
+                        <Icon active name="navigate" />
+                    </Button>
+                    <Button>
+                        <Icon name="camera" style={{ color: "#FFFFFF" }} />
+                    </Button>
+                    <Button>
+                        <Icon name="person" style={{ color: "#FFFFFF" }} />
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </>
     );
-  }
 }
