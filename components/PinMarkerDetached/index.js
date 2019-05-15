658c5fc525db5559f7b201b9f9aa66005c9c4ab5
David Adams <daveadamsdev@gmail.com>
2019-05-15 11:58:30 +0100

    food icon added

--- components/PinMarkerDetached/index.js
+++ components/PinMarkerDetached/index.js
@@ -20,7 +20,8 @@ const iconSet = {
   Building: require("../../assets/images/icons/Building.png"),
   Culture: require("../../assets/images/icons/Culture.png"),
   Market: require("../../assets/images/icons/Market.png"),
-  Event: require("../../assets/images/icons/Event.png")
+  Event: require("../../assets/images/icons/Event.png"),
+  FoodDrink: require("../../assets/images/icons/Food.png")
 };
 
 const styles = StyleSheet.create({
