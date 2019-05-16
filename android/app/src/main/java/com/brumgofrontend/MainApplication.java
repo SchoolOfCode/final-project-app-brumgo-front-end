fa1243928df83ac8ef149e1fd225b93c08c8c81e
Manu Magalhaes <manu.magalhaes@ymail.com>
2019-05-16 12:24:38 +0100

    added firebase for push notifications

--- android/app/src/main/java/com/brumgofrontend/MainApplication.java
+++ android/app/src/main/java/com/brumgofrontend/MainApplication.java
@@ -3,6 +3,7 @@ package com.brumgofrontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
           //new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf(BuildConfig.VR_PLATFORM)),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
