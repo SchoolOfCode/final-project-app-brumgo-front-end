035a5f09b0e430c610b38985c5a806148d430d08
David Adams <daveadamsdev@gmail.com>
2019-05-23 16:22:19 +0100

    rebase phase 2

--- components/CatToggle/index.js
+++ components/CatToggle/index.js
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     marginBottom: 2
   },
   container: {
+    flex: 1,
     alignContent: "center",
     alignItems: "center"
   }
--- components/MainAppComponent/index.js
+++ components/MainAppComponent/index.js
@@ -43,6 +43,7 @@ function MapScreen(props) {
 }
 
 function ListScreen(props) {
+  const [searching, setSearching] = useState(false);
   return (
     <ViewContainer {...props}>
       <PickerCategory />
