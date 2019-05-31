19ebd64f5e20e19ff25a07a53c602c336b06fdaa
David Adams <daveadamsdev@gmail.com>
2019-05-31 14:58:04 +0100

    Duplicate import UserProfiles removed/fixed in MainAppComponent

--- components/MainAppComponent/index.js
+++ components/MainAppComponent/index.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import {
-    createMaterialTopTabNavigator,
-    createAppContainer
+  createMaterialTopTabNavigator,
+  createAppContainer
 } from "react-navigation";
-import UserProfile from "../UserProfile"
 
 import FlatList from "../FlatList";
 import FooterSection from "../FooterSection";
@@ -14,98 +13,96 @@ import GeoPopping from "../GeoPopping";
 import PickerCategory from "../PickerCategory";
 import UserProfile from "../UserProfile";
 import usePoiFilter from "../../utils/hooks/usePoiFilter";
-import SocialMediaFAB from "../SocialMediaFAB"
-
+import SocialMediaFAB from "../SocialMediaFAB";
 
 function ViewContainer(props) {
-    return (
-        <View style={styles.container}>
-            {props.children}
-            <View style={styles.footer}>
-                <FooterSection {...props} />
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      {props.children}
+      <View style={styles.footer}>
+        <FooterSection {...props} />
+      </View>
+    </View>
+  );
 }
 
 function MapScreen(props) {
-
-    return (
-        <ViewContainer {...props}>
-            <View style={styles.map}>
-                <GeoPopping />
-                <MapDisplay />
-      <SocialMediaFAB />
-            </View>
-        </ViewContainer>
-    );
+  return (
+    <ViewContainer {...props}>
+      <View style={styles.map}>
+        <GeoPopping />
+        <MapDisplay />
+        <SocialMediaFAB />
+      </View>
+    </ViewContainer>
+  );
 }
 
 function ListScreen(props) {
-    return (
-        <ViewContainer {...props}>
-            <PickerCategory />
-            <View style={styles.list}>
-                <FlatList />
-            </View>
-        </ViewContainer>
-    );
+  return (
+    <ViewContainer {...props}>
+      <PickerCategory />
+      <View style={styles.list}>
+        <FlatList />
+      </View>
+    </ViewContainer>
+  );
 }
 
 const TabNavigator = createMaterialTopTabNavigator(
-    {
-        Map: {
-            screen: MapScreen,
-            navigationOptions: {
-                tabBarLabel: "Map"
-            }
-        },
-        List: {
-            screen: ListScreen,
-            navigationOptions: {
-                tabBarLabel: "List"
-            }
-        }
+  {
+    Map: {
+      screen: MapScreen,
+      navigationOptions: {
+        tabBarLabel: "Map"
+      }
     },
-    {
-        tabBarOptions: {
-            activeTintColor: "#FFFFFF",
-            inactiveTintColor: "#D3D3D3",
-            showLabel: true,
-            style: {
-                backgroundColor: "#E12B38"
-            }
-        }
+    List: {
+      screen: ListScreen,
+      navigationOptions: {
+        tabBarLabel: "List"
+      }
     }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#FFFFFF",
+      inactiveTintColor: "#D3D3D3",
+      showLabel: true,
+      style: {
+        backgroundColor: "#E12B38"
+      }
+    }
+  }
 );
 
 export default createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: "100%"
-    },
+  container: {
+    flex: 1,
+    width: "100%"
+  },
 
-    map: {
-        // flex: 7,
-        height: "90%",
-        width: "100%"
-    },
-    list: {
-        height: "81%",
-        width: "100%"
-    },
-    footer: {
-        height: "10%",
-        width: "100%",
-        position: "relative"
-    },
-    search: {
-        // flex: 1,
-        top: -50,
-        height: "10%",
-        width: "100%",
-        position: "relative"
-    }
+  map: {
+    // flex: 7,
+    height: "90%",
+    width: "100%"
+  },
+  list: {
+    height: "81%",
+    width: "100%"
+  },
+  footer: {
+    height: "10%",
+    width: "100%",
+    position: "relative"
+  },
+  search: {
+    // flex: 1,
+    top: -50,
+    height: "10%",
+    width: "100%",
+    position: "relative"
+  }
 });
