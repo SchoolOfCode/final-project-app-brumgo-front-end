6f516aaa18b687893634befa8c8424541db1c69d
David Adams <daveadamsdev@gmail.com>
2019-06-05 14:44:24 +0100

    exported loginapp index (forgotted b4)

--- components/LoginApp/index.js
+++ components/LoginApp/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import LoginTabs from "./LoginTabs";
 import MasterNavContext from "../NavigationContext";
 
-const LoginApp = props => {
+export default (LoginApp = props => {
   return (
     <MasterNavContext.Provider value={props.navigation}>
       <LoginTabs />
     </MasterNavContext.Provider>
   );
-};
+});
