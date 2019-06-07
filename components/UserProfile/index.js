ba3c75f869b38e3dd7037cff5051506649256eb9
David Adams <daveadamsdev@gmail.com>
2019-06-07 14:00:15 +0100

    Navigation to UserProfile bug fixed - User Profile needed to be provided with the PoiContextProvider

--- components/UserProfile/index.js
+++ components/UserProfile/index.js
@@ -4,114 +4,108 @@ import { Avatar, Icon, ListItem } from "react-native-elements";
 import FooterSection from "../FooterSection";
 import NavContext from "../NavigationContext";
 import Header from "../HeaderSection";
-
+import { PoiContextProvider } from "../PoiContextProvider";
 
 export default (UserProfile = props => {
   return (
-    <NavContext.Provider value={props.navigation}>
-      <Header />
-      <ImageBackground
-        source={require("../LoadingPage/brum.jpg")}
-        style={styles.background}
-      >
-        <View style={styles.overlay}>
-          <View style={styles.container}>
-            <View style={styles.top}>
-              <Icon
-                name="user-circle"
-                type="font-awesome"
-                color="#FFFFFF"
-                size={80}
-              />
-              <Text style={styles.name}>Joe Bloggs</Text>
-            </View>
-
-            <View style={styles.middle}>
-              <Icon
-                name="user-plus"
-                type="font-awesome"
-                color="#FCC133"
-                raised
-              />
-              <Icon name="edit" type="font-awesome" color="#FCC133" size={35} raised />
-              <Icon
-                name="share-alt"
-                type="font-awesome"
-                color="#FCC133"
-                raised
-              />
-            </View>
-
-            <View style={styles.bottom}>
-              <Icon
-                name="credit-card"
-                type="font-awesome"
-                color="#FFFFFF"
-              />
-              <Text
-                style={{
-                  fontWeight: "bold",
-                  color: "#FFFFFF"
-                }}
-              >
-                Offers & Discounts
-              </Text>
-              <Icon
-                name="chevron-right"
-                type="font-awesome"
-                color="#FFFFFF"
-              />
-            </View>
-            <View style={styles.bottom}>
-              <Icon
-                name="star"
-                type="font-awesome"
-                color="#FFFFFF"
-              />
-              <Text
-                style={{
-                  fontWeight: "bold",
-                  color: "#FFFFFF"
-                }}
-              >
-                Favourite Places
-              </Text>
-              <Icon
-                name="chevron-right"
-                type="font-awesome"
-                color="#FFFFFF"
-              />
-            </View>
-
-            <View style={styles.bottom}>
-              <Icon
-                name="check"
-                type="font-awesome"
-                color="#FFFFFF"
-              />
-              <Text
-                style={{
-                  fontWeight: "bold",
-                  color: "#FFFFFF"
-                }}
-              >
-                Locations Visited
-              </Text>
-              <Icon
-                name="chevron-right"
-                type="font-awesome"
-
-                color="#FFFFFF"
+    <PoiContextProvider>
+      <NavContext.Provider value={props.navigation}>
+        <Header />
+        <ImageBackground
+          source={require("../LoadingPage/brum.jpg")}
+          style={styles.background}
+        >
+          <View style={styles.overlay}>
+            <View style={styles.container}>
+              <View style={styles.top}>
+                <Icon
+                  name="user-circle"
+                  type="font-awesome"
+                  color="#FFFFFF"
+                  size={80}
                 />
+                <Text style={styles.name}>Joe Bloggs</Text>
+              </View>
+
+              <View style={styles.middle}>
+                <Icon
+                  name="user-plus"
+                  type="font-awesome"
+                  color="#FCC133"
+                  raised
+                />
+                <Icon
+                  name="edit"
+                  type="font-awesome"
+                  color="#FCC133"
+                  size={35}
+                  raised
+                />
+                <Icon
+                  name="share-alt"
+                  type="font-awesome"
+                  color="#FCC133"
+                  raised
+                />
+              </View>
+
+              <View style={styles.bottom}>
+                <Icon name="credit-card" type="font-awesome" color="#FFFFFF" />
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    color: "#FFFFFF"
+                  }}
+                >
+                  Offers & Discounts
+                </Text>
+                <Icon
+                  name="chevron-right"
+                  type="font-awesome"
+                  color="#FFFFFF"
+                />
+              </View>
+              <View style={styles.bottom}>
+                <Icon name="star" type="font-awesome" color="#FFFFFF" />
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    color: "#FFFFFF"
+                  }}
+                >
+                  Favourite Places
+                </Text>
+                <Icon
+                  name="chevron-right"
+                  type="font-awesome"
+                  color="#FFFFFF"
+                />
+              </View>
+
+              <View style={styles.bottom}>
+                <Icon name="check" type="font-awesome" color="#FFFFFF" />
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    color: "#FFFFFF"
+                  }}
+                >
+                  Locations Visited
+                </Text>
+                <Icon
+                  name="chevron-right"
+                  type="font-awesome"
+                  color="#FFFFFF"
+                />
+              </View>
             </View>
+            <FooterSection />
           </View>
-          <FooterSection/>
-        </View>
-      </ImageBackground>
-    </NavContext.Provider>
+        </ImageBackground>
+      </NavContext.Provider>
+    </PoiContextProvider>
   );
 });
-
 
 const styles = StyleSheet.create({
   background: {
@@ -139,7 +133,7 @@ const styles = StyleSheet.create({
     marginTop: "10%",
     width: 180,
     borderWidth: 3,
-    borderColor: "#FFFFFF",
+    borderColor: "#FFFFFF"
   },
   middle: {
     backgroundColor: "transparent",
@@ -149,7 +143,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 60,
     margin: 10,
-    padding: 10,
+    padding: 10
   },
   bottom: {
     backgroundColor: "transparent",
