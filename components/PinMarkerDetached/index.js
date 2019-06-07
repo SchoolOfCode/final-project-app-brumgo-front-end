f7b14a598e22338d459c0783d56d5d5e44fb762a
David Adams <daveadamsdev@gmail.com>
2019-06-07 21:03:25 +0100

    Dialogue from Map <Callout> now has the option 'navigate to' instead of 'read more' navigation from this working

--- components/PinMarkerDetached/index.js
+++ components/PinMarkerDetached/index.js
@@ -1,14 +1,6 @@
-import React from "react";
-import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  TouchableHighlight,
-  Button
-} from "react-native";
+import React, { useState, useContext } from "react";
+import { StyleSheet, Image, Text } from "react-native";
 import { Marker, Callout } from "react-native-maps";
-import pois from "../../data/digbethPois";
 import Dialog, {
   DialogTitle,
   DialogContent,
@@ -16,7 +8,9 @@ import Dialog, {
   DialogButton,
   ScaleAnimation
 } from "react-native-popup-dialog";
-import ReadMore from "../ReadMore";
+
+import { PoiContext } from "../PoiContextProvider";
+
 import iconSet from "../../assets/images/icons/iconSet";
 
 const styles = StyleSheet.create({
@@ -53,136 +47,132 @@ const styles = StyleSheet.create({
   }
 });
 
-keyExtractor = (item, index) => index.toString();
+export default function PinMarkerDetached(props) {
+  const [{ directionsData }, { setDestination }] = useContext(PoiContext);
 
-export default class PinMarkerDetached extends React.Component {
-  state = {
+  const [state, setState] = useState({
     customBackgroundDialog: false,
     defaultAnimationDialog: false,
     scaleAnimationDialog: false,
     slideAnimationDialog: false,
     readMore: true
-  };
+  });
 
-  render() {
-    const {
-      poiName,
-      poiLocation,
-      category,
-      poiTrivia,
-      extraPoiTrivia,
-      urlExtra
-    } = this.props.poi;
-    return (
-      <Marker
-        key={this.props.key}
-        title={poiName}
-        coordinate={{
-          latitude: poiLocation.lat,
-          longitude: poiLocation.lng
+  const {
+    poiName,
+    poiLocation,
+    category,
+    poiTrivia,
+    extraPoiTrivia,
+    urlExtra
+  } = props.poi;
+  return (
+    <Marker
+      key={props.key}
+      title={poiName}
+      coordinate={{
+        latitude: poiLocation.lat,
+        longitude: poiLocation.lng
+      }}
+    >
+      <Image style={{ height: 20, width: 20 }} source={iconSet[category]} />
+      <Callout
+        style={{
+          width: 100,
+          backgroundColor: "#ffffff"
+        }}
+        tooltip={false}
+        onPress={() => {
+          setState({
+            ...state,
+            defaultAnimationDialog: true
+          });
         }}
       >
-        <Image style={{ height: 20, width: 20 }} source={iconSet[category]} />
-        <Callout
+        <Text
           style={{
-            width: 100,
-            backgroundColor: "#ffffff"
-          }}
-          tooltip={false}
-          onPress={() => {
-            this.setState({
-              defaultAnimationDialog: true
-            });
+            fontWeight: "bold"
           }}
         >
-          <Text
+          {poiName}
+        </Text>
+      </Callout>
+
+      <Dialog
+        overlayBackgroundColor={"rgba(0, 0, 0, 0.3)"}
+        dialogAnimation={new ScaleAnimation()}
+        onHardwareBackPress={() => {
+          console.log("onHardwareBackPress");
+          setState({
+            ...state,
+            scaleAnimationDialog: false
+          });
+          return true;
+        }}
+        onDismiss={() => {
+          setState({ ...state, defaultAnimationDialog: false });
+        }}
+        width={0.9}
+        visible={state.defaultAnimationDialog}
+        rounded
+        actionsBordered
+        dialogTitle={
+          <DialogTitle
+            title={poiName}
             style={{
-              fontWeight: "bold"
-            }}
-          >
-            {poiName}
-          </Text>
-        </Callout>
-
-        <Dialog
-          overlayBackgroundColor={"rgba(0, 0, 0, 0.3)"}
-          dialogAnimation={new ScaleAnimation()}
-          onHardwareBackPress={() => {
-            console.log("onHardwareBackPress");
-            this.setState({
-              scaleAnimationDialog: false
-            });
-            return true;
-          }}
-          onDismiss={() => {
-            this.setState({
-              defaultAnimationDialog: false
-            });
-          }}
-          width={0.9}
-          visible={this.state.defaultAnimationDialog}
-          rounded
-          actionsBordered
-          dialogTitle={
-            <DialogTitle
-              title={poiName}
-              style={{
-                backgroundColor: "#FFFFFF",
-                margin: 10,
-                color: "#FCC133"
-      }}
-              hasTitleBar={true}
-
-              align="left"
-            />
-          }
-          footer={
-            <DialogFooter>
-              <DialogButton
-                text={this.state.readMore ? "Read More" : "Back"}
-                textStyle={{ color: "#E12B38"}}
-                activeOpacity={0.6}
-                onPress={() => {
-                  this.setState({
-                    readMore: !this.state.readMore
-                  });
-                }}
-                key="button-1"
-              />
-              <DialogButton
-                text="Done"
-                bordered
-                textStyle={{ color: "#E12B38" }}
-                activeOpacity={0.6}
-
-                onPress={() => {
-                  this.setState({
-                    defaultAnimationDialog: false,
-                    readMore: true
-                  });
-                }}
-                key="button-3"
-              />
-            </DialogFooter>
-          }
-        >
-          <DialogContent
-            style={{
-
               backgroundColor: "#FFFFFF",
-              margin: 10, 
+              margin: 10,
+              color: "#FCC133"
             }}
-          >
-            <Text>
-              {this.state.readMore ? (
-                poiTrivia
-              ) : (
-                <ReadMore extraPoiTrivia={extraPoiTrivia} urlExtra={urlExtra} />
-              )}
-            </Text>
-          </DialogContent>
-        </Dialog>
-      </Marker>
-    );
-  }
+            hasTitleBar={true}
+            align="left"
+          />
+        }
+        footer={
+          <DialogFooter>
+            <DialogButton
+              text={`Navigate to`}
+              textStyle={{ color: "#E12B38" }}
+              activeOpacity={0.6}
+              onPress={() => {
+                setDestination({
+                  latitude: poiLocation.lat,
+                  longitude: poiLocation.lng
+                });
+                setState({
+                  ...state,
+                  defaultAnimationDialog: false,
+                  readMore: true
+                });
+              }}
+              key="button-1"
+            />
+            <DialogButton
+              text="Done"
+              bordered
+              textStyle={{ color: "#E12B38" }}
+              activeOpacity={0.6}
+              onPress={() => {
+                setState({
+                  ...state,
+                  defaultAnimationDialog: false,
+                  readMore: true
+                });
+              }}
+              key="button-3"
+            />
+          </DialogFooter>
+        }
+      >
+        <DialogContent
+          style={{
+            backgroundColor: "#FFFFFF",
+            margin: 10
+          }}
+        >
+          <Text>{poiTrivia}</Text>
+        </DialogContent>
+      </Dialog>
+    </Marker>
+  );
 }
